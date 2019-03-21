e54a8aa388834df6f2ea2005b035d1b40a338e9d
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-22 00:16:49 +0100

    removing damp data

--- calendar_blog/views.py
+++ calendar_blog/views.py
@@ -1,86 +1,20 @@
 from django.shortcuts import render
 import json
 
-months = [
+posts = [
     {
-        'month': 'CoreyMS'
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
     },
     {
-        'month': 'CoreyMS'
-    },
-    {
-        'month': 'CoreyMS'
-    },
-    {
-        'month': 'CoreyMS'
-    },
-    {
-        'month': 'CoreyMS'
-    },
-    {
-        'month': 'CoreyMS'
-    },
-    {
-        'month': 'CoreyMS'
-    },
-    {
-        'month': 'CoreyMS'
-    },
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
 ]
-
-
-# calendar_post = {
-#     {
-#         'month': 'January',
-#         '1':'green',
-#         '2':'green',
-#         '3':'green',
-#         '4':'green',
-#         '5':'green',
-#         '6':'green',
-#         '7':'green',
-#         '8':'green',
-#         '9':'green',
-#         '10':'green',
-#         '12':'green',
-#         '13':'green',
-#         '14':'green',
-#         '15':'green',
-#         '16':'green',
-#         '17':'green',
-#         '18':'green',
-#         '19':'green',
-#         '20':'green',
-#         '21':'green',
-#         '22':'green',
-#         '23':'green',
-#         '24':'green',
-#         '25':'green',
-#         '26':'green',
-#         '27':'green',
-#         '28':'green',
-#         '29':'green',
-#         '30':'green',
-#         '31':'green'
-#     }
-# }
-
-# jauary1 = [
-#     {
-#         'month':'January',
-#         'day':'1',
-#         'done':'white',
-#         'hours':'3',
-#     },
-#     {
-#         'month':'January',
-#         'day':'2',
-#         'done':'red',
-#         'hours':'3',
-#         'title':'Deutsch Learnen'
-#     }
-# ]
-# months = ["Yanvar", "Fevral", "Mart"]
 
 with open('calendar_blog/deutsch_oy_1.json') as f:
     json_01_ = json.load(f)
