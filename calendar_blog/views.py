f8bdeaeef6c5be46dd62aeb21a701d66f5399dcd
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-22 00:14:23 +0100

    full dynamic calendar with json data

--- calendar_blog/views.py
+++ calendar_blog/views.py
@@ -82,11 +82,42 @@ months = [
 # ]
 # months = ["Yanvar", "Fevral", "Mart"]
 
-with open('calendar_blog/yanvar19_deutsch.json') as f:
+with open('calendar_blog/deutsch_oy_1.json') as f:
     json_01_ = json.load(f)
 
-with open('calendar_blog/fevral19_deutsch.json') as f:
+with open('calendar_blog/deutsch_oy_2.json') as f:
     json_02_ = json.load(f)
+
+with open('calendar_blog/deutsch_oy_3.json') as f:
+    json_03_ = json.load(f)
+
+with open('calendar_blog/deutsch_oy_4.json') as f:
+    json_04_ = json.load(f)
+
+with open('calendar_blog/deutsch_oy_5.json') as f:
+    json_05_ = json.load(f)
+
+with open('calendar_blog/deutsch_oy_6.json') as f:
+    json_06_ = json.load(f)
+
+with open('calendar_blog/deutsch_oy_7.json') as f:
+    json_07_ = json.load(f)
+
+with open('calendar_blog/deutsch_oy_8.json') as f:
+    json_08_ = json.load(f)
+
+with open('calendar_blog/deutsch_oy_9.json') as f:
+    json_09_ = json.load(f)
+
+with open('calendar_blog/deutsch_oy_10.json') as f:
+    json_10_ = json.load(f)
+
+with open('calendar_blog/deutsch_oy_11.json') as f:
+    json_11_ = json.load(f)
+
+with open('calendar_blog/deutsch_oy_12.json') as f:
+    json_12_ = json.load(f)
+
 
 
 def home(request):
@@ -102,6 +133,16 @@ def about(request):
 def calendarr(request):
     context = {
     'january_19':json_01_["deutsch"],
-    'fevral_19':json_02_["deutsch"]
+    'fevral_19':json_02_["deutsch"],
+    'mart19':json_03_["deutsch"],
+    'april19':json_04_["deutsch"],
+    'may19':json_05_["deutsch"],
+    'iyun19':json_06_["deutsch"],
+    'iyul19':json_07_["deutsch"],
+    'avgust19':json_08_["deutsch"],
+    'sentabr19':json_09_["deutsch"],
+    'oktabr19':json_10_["deutsch"],
+    'noyabr19':json_11_["deutsch"],
+    'dekabr19':json_12_["deutsch"]
     }
     return render(request, 'calendar_blog/calendar.html', context)
