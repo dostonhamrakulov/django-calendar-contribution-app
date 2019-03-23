08987b5c99990b6269dc86ee4b5706d4ad3e08e3
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-23 11:52:52 +0100

    changing urls

--- calendar_blog/urls.py
+++ calendar_blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('calendar/', views.calendarr, name='blog-calendar'),
-    path('calendar/', views.calendarr, name='blog-contact'),
-    path('calendar/', views.calendarr, name='blog-source_code'),
+    path('contact/', views.contact, name='blog-contact'),
+    path('source_code/', views.source_code, name='blog-source_code'),
 ]
