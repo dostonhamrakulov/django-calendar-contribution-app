181ce0c3788ae1628463ec92246b523d676d77a4
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-04-01 22:16:59 +0200

    March.py

---
+++ calendar_blog/data_3.py
@@ -0,0 +1,20 @@
+
+
+posts = [
+    {
+    "month":"February",
+    "day":"1",
+    "done":"green",
+    "hours":"3",
+    "title":"Deutsch Learnen",
+    "color":"white", "new2":"value2", "new2":"value2"
+    },
+    {
+
+    "day":"2",
+    "done":"green",
+    "hours":"3",
+    "title":"Deutsch Learnen",
+    "color":"white", "new2":"value2"
+    }
+]
